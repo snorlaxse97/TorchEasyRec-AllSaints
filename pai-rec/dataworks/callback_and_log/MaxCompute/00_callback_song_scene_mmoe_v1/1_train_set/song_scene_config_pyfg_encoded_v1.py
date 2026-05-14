5c7c31fa982f192a61bf5fcb682f7c4db538d919
snorlaxse97 <snorlaxse@163.com>
2026-05-14 15:46:42 +0800

    del pai-rec

--- pai-rec/dataworks/callback_and_log/MaxCompute/00_callback_song_scene_mmoe_v1/1_train_set/song_scene_config_pyfg_encoded_v1.py
+++
@@ -1,8 +0,0 @@
-from pyfg055 import offline_pyfg
-offline_pyfg.run(
-  o,
-  input_table="song_scene_trainning_set_v1",
-  output_table="song_scene_config_pyfg_encoded_v1",
-  fg_json_file="song_scene_config_fg.json",
-  partition_value=args['bizdate']
-)
--- pai-rec/dataworks/ps_dfm_v6/MaxCompute/rank_v1/ps_dfm_v6_dfm_config_pyfg_encoded_v1.py
+++
@@ -1,8 +0,0 @@
-from pyfg055 import offline_pyfg
-offline_pyfg.run(
-  o,
-  input_table="prd_fs_ps_dfm_v6_dfm_v1_training_set",
-  output_table="ps_dfm_v6_dfm_config_pyfg_encoded_v1",
-  fg_json_file="ps_dfm_v6_dfm_config_fg_v1.json",
-  partition_value=args['bizdate']
-)
